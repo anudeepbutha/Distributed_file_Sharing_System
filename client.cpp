0ffbf46865442b7ac1136c953600e8dc2258ab59
anudeepbutha <111654636+anudeepbutha@users.noreply.github.com>
2025-10-02 02:31:00 +0530

    Add files via upload

--- client.cpp
+++ client.cpp
@@ -1,8 +1,11 @@
 #include <bits/stdc++.h>
 #include <fcntl.h>
 #include <netdb.h>
+#include <openssl/sha.h>
 #include <sys/socket.h>
 #include <unistd.h>
+
+#define CHUNK_SIZE 524288
 
 using namespace std;
 
@@ -22,13 +25,32 @@ string send_message(int sockfd, vector<string> words) {
     return buffer;
 }
 
+string ssend_message(int sockfd, string msg) {
+    char buffer[1024];
+    send(sockfd, msg.c_str(), msg.size(), 0);
+    ssize_t n = recv(sockfd, buffer, sizeof(buffer) - 1, 0);
+    buffer[n]='\0';
+    return buffer;
+}
+
+string filepath_to_filename(string& file_path) {
+    size_t pos = file_path.find_last_of('/');
+
+    if (pos != string::npos)
+        return file_path.substr(pos + 1);
+
+    return file_path;
+}
+
 int main(int argc, char *argv[]) {
     if (argc < 3) {
         cout << "Invalid Arguments" << endl;
         return 0;
     }
 
-    char* tracker_fname = argv[1];
+    char* client_details = argv[1];
+    
+    char* tracker_fname = argv[2];
     int tracker_ports[2];
     ifstream file(tracker_fname);
     string line;
@@ -66,9 +88,9 @@ int main(int argc, char *argv[]) {
     while (1) {
         string line_read;
         cout << "$>";
-		getline(cin,line_read);
-		vector<string> words;
-		char *token = strtok(const_cast<char*>(line_read.c_str()), " ");
+        getline(cin,line_read);
+        vector<string> words;
+        char *token = strtok(const_cast<char*>(line_read.c_str()), " ");
 
         while (token != NULL) {
             words.push_back(token);
@@ -106,11 +128,77 @@ int main(int argc, char *argv[]) {
             cout << "Available groups are\n" << reply << endl;
         }
         else if (words[0] == "accept_request" && words.size() == 3) {
-
+            string reply = send_message(sockfd, words);
+            cout << "Available groups are\n" << reply << endl;
         }
         else if (words[0] == "logout" && words.size() == 1) {
             string reply = send_message(sockfd, words);
             cout << "Recieved from tracker: " << reply << endl;
+        }
+        else if (words[0] == "quit" && words.size() == 1) {
+            break;
+        }
+        else if (words[0] == "upload_file" && words.size() == 3) {
+            string group_id = words[1];
+            string file_path = words[2];
+            string filename = filepath_to_filename(file_path);
+            int fd = open(file_path.c_str(), O_RDONLY);
+            if (fd < 0) {
+                cout << "No such file exists" << endl;
+                continue;
+            }
+
+            off_t file_size = lseek(fd, 0, SEEK_END);
+            lseek(fd, 0, SEEK_SET);
+            if (file_size < 0)
+                error("Issue with file size");
+            int chunks_count = file_size / CHUNK_SIZE;
+            int last_chunk_size = file_size % CHUNK_SIZE;
+            if (last_chunk_size>0)
+                chunks_count++;
+            cout << "Uploading file: " << filename << endl;
+            cout << "File Size: " << file_size << endl;
+            cout << "Number of chunks - " << chunks_count << endl;
+
+            vector<string> chunk_hashes;
+            char hash_buffer[CHUNK_SIZE];
+            off_t bytes_read = 0;
+
+            SHA_CTX file_ctx;
+            SHA1_Init(&file_ctx);
+
+            while(bytes_read < file_size){
+                memset(hash_buffer, 0, sizeof(hash_buffer));
+                ssize_t n = read(fd, hash_buffer, CHUNK_SIZE);
+                if (n<=0) break;
+
+                SHA1_Update(&file_ctx, hash_buffer, n);
+
+                unsigned char hash_raw[SHA_DIGEST_LENGTH];
+                SHA1(reinterpret_cast<const unsigned char *>(hash_buffer), n, hash_raw);
+
+                char output_buffer[41];
+                for (int i = 0; i < SHA_DIGEST_LENGTH; i++)
+                    sprintf(output_buffer + (i * 2), "%02x", hash_raw[i]);
+                output_buffer[SHA_DIGEST_LENGTH * 2] = '\0';
+                chunk_hashes.push_back(output_buffer);
+                bytes_read += n;
+            }
+
+            unsigned char file_hash_raw[SHA_DIGEST_LENGTH];
+            SHA1_Final(file_hash_raw, &file_ctx);
+
+            char file_hash[41];
+            for (int i = 0; i < SHA_DIGEST_LENGTH; i++)
+                sprintf(file_hash + (i * 2), "%02x", file_hash_raw[i]);
+            file_hash[SHA_DIGEST_LENGTH * 2] = '\0';
+            close(fd);
+            string msg="upload_file " + group_id + " " + filename + " " + file_path + " " +
+            to_string(file_size) + " " + to_string(chunks_count) + " " + file_hash + " ";
+            for (int i=0; i<chunk_hashes.size(); i++)
+                msg += chunk_hashes[i] + " ";
+            string reply = ssend_message(sockfd, msg);
+            cout << reply << endl;
         }
         else {
             cout << "Invalid Arguments" << endl;
--- tracker.cpp
+++ tracker.cpp
@@ -2,6 +2,7 @@
 #include <bits/stdc++.h>
 #include <fcntl.h>
 #include <netdb.h>
+#include <openssl/sha.h>
 #include <sys/socket.h>
 #include <thread>
 #include <unistd.h>
@@ -20,53 +21,48 @@ struct user_info {
 
 struct group {
     string owner;
-    set<string> member;
-    set<string> request;
+    vector<string> member;
+    vector<string> request;
+    vector<string> filesinGroup;
 };
-
 
 struct tracker {
     string ip;
     int port;
 };
 
+struct file {
+    string file_name;
+    long file_size;
+    vector<pair<string,bool>> file_users;	
+    vector<string> chunk_hashes;
+    string file_hash;
+};
+
 map<string, user_info> users;
 map<int, string> session_map;
 map<string, group> group_map;
+map<string, file> file_map;
 
-// void sync_function(const string &msg, int sync_port) {
-//     char buffer[256];
+void quit_func(int i){
+    string input;
+    while(1){
+        getline(cin,input);
+        if(input == "quit")
+            exit(0);
+    }
+}
 
-//     int sync_sockfd = socket(AF_INET, SOCK_STREAM, 0);
-//     if (sync_sockfd < 0)
-//         return;
+void cmd_log() {
 
-//     struct sockaddr_in sync_track_add;
-//     bzero((char *) &sync_track_add, sizeof(sync_track_add));
-//     sync_track_add.sin_family = AF_INET;
-//     sync_track_add.sin_port = htons(sync_port);
+}
 
-//     if (inet_pton(AF_INET, "127.0.0.1", &sync_track_add.sin_addr) <= 0) {
-//         perror("invalid sync server IP");
-//         close(sync_sockfd);
-//         return;
-//     }
-
-//     if (connect(sync_sockfd,(struct sockaddr *) &sync_track_add,sizeof(sync_track_add)) < 0)
-//         return;
-
-//     cout << "connection established" << endl;
-
-//     ssize_t sent = send(sync_sockfd, msg.c_str(), msg.size(), 0);
-
-//     cout << "connection established 2" << endl;
-
-//     ssize_t bytes_received = recv(sync_sockfd, buffer, sizeof(buffer)-1, 0);
-
-//     cout << buffer << endl;
-
-//     close(sync_sockfd);
-// }
+bool isUserPresent(vector<pair<string,bool>> file_users, string user_name) {
+    for (auto itr: file_users)
+        if(itr.first == user_name)
+            return true;
+    return false;
+}
 
 void client_handler(int newsockfd) {
     while(1) {
@@ -79,7 +75,7 @@ void client_handler(int newsockfd) {
         }
 
         vector<string> words;
-		char *token = strtok(buffer, " ");
+        char *token = strtok(buffer, " ");
 
         while (token != NULL) {
             words.push_back(token);
@@ -89,11 +85,7 @@ void client_handler(int newsockfd) {
         string msg;
         if (words[0] == "create_user") {
             if (users.find(words[1]) == users.end()) {
-                struct user_info uc;
-                uc.username = words[1];
-                uc.password = words[2];
-                uc.login_status = false;
-                users[words[1]] = uc;
+                users[words[1]] = {words[1], words[2], false};
                 msg = "User created successfully";
             }
             else {
@@ -142,22 +134,47 @@ void client_handler(int newsockfd) {
             if (session_map.find(newsockfd) == session_map.end()){
                 msg = "Please login first";
             }
-            else if(group_map.find(words[1]) != group_map.end()) {
-                group group_info = group_map[words[1]];
-                if (group_info.owner == session_map[newsockfd])
-                    msg = "You are the owner of the group";
-                else {
-                    group_info.request.insert(session_map[newsockfd]);
-                    msg = "Join request sent to owner";
-                }
+            else if (group_map.find(words[1]) == group_map.end()) {
+                msg = "group doesnot exist";
             }
             else {
-                msg = "group doesnot exist";
+                string curr_user = session_map[newsockfd];
+                group group_info = group_map[words[1]];
+                for(int i=0; i<group_info.member.size(); i++)
+                    if(group_info.member[i] == curr_user) {
+                        msg = "You are already a member of this group";
+                        send(newsockfd, msg.c_str(), msg.size(), 0);
+                        continue;
+                    }
+                if (group_info.owner == curr_user)
+                    msg = "You are the owner of the group";
+                else {
+                    group_info.request.push_back(curr_user);
+                    msg = "Join request sent to owner";
+                }
             }
             send(newsockfd, msg.c_str(), msg.size(), 0);
         }
         else if (words[0] == "leave_group") {
-            
+            if (session_map.find(newsockfd) == session_map.end()){
+                msg = "Please login first";
+            }
+            else if (group_map.find(words[1]) == group_map.end()) {
+                msg = "group doesnot exist";
+            }
+            else {
+                string curr_user = session_map[newsockfd];
+                group gp = group_map[words[1]];
+                auto itr = find(gp.member.begin(), gp.member.end(), curr_user);
+                if (itr != gp.member.end()) {
+                    msg = "You are not a member of this group";
+                }
+                else {
+                    gp.member.erase(itr);
+                    msg = "Group left";
+                }
+            }
+            send(newsockfd, msg.c_str(), msg.size(), 0);
         }
         else if (words[0] == "list_groups") {
             for (const auto &pair : group_map)
@@ -185,15 +202,83 @@ void client_handler(int newsockfd) {
             send(newsockfd, msg.c_str(), msg.size(), 0);
         }
         else if (words[0] == "accept_requests") {
-
+            if (session_map.find(newsockfd) == session_map.end()){
+                msg = "Please login first";
+            }
+            else if (users.find(words[2]) == users.end()) {
+                msg = "No such user found";
+            }
+            else if (group_map.find(words[1]) == group_map.end()) {
+                msg = "group doesnot exist";
+            }
+            else if (session_map[newsockfd] == group_map[words[1]].owner){
+                msg = "Please login with owner of the group";
+            }
+            else {
+                struct group gp = group_map[words[1]];
+                bool flag=false;
+                for(int i=0;i<gp.request.size(); i++)
+                    if(gp.request[i] == words[2]) {
+                        gp.request.erase(find(gp.request.begin(),gp.request.end(),words[2]));
+                        gp.member.push_back(words[2]);
+                        msg = "Request Accepted";
+                        flag=true;
+                        break;
+                    }
+                if (flag)
+                    msg = "No pending request found for the user";
+            }
+            send(newsockfd, msg.c_str(), msg.size(), 0);
         }
         else if (words[0] == "logout") {
             if (session_map.find(newsockfd) != session_map.end()){
+                string curr_user = session_map[newsockfd];
+                user_info uc = users[curr_user];
+                uc.login_status=false;
                 session_map.erase(newsockfd);
                 msg = "User Logged out";
             }
             else {
                 msg = "No user logged in";
+            }
+            send(newsockfd, msg.c_str(), msg.size(), 0);
+        }
+        else if (words[0] == "upload_file") {
+            string group_id = words[1];
+            string curr_user = session_map[newsockfd];
+            string msg;
+            if(group_map.find(group_id) == group_map.end()){
+                msg = group_id + " group ID doesn't exist";
+            }
+            else{
+                // if(!groups[words[1]]->isFileInGroup(words[3])){
+                // 	groups[words[1]]->filesinGroup.push_back(words[3]);
+                // }
+                if(file_map.find(words[2]) != file_map.end()){
+                    // check for user already present
+                    file fl = file_map[words[2]];
+                    if(isUserPresent(fl.file_users, curr_user)){
+                        msg="file already has been uploaded by user ID: "+curr_user;
+                    }
+                    else{
+                        fl.file_users.push_back({curr_user,1});
+                        // users[commands[2]]->fnameToPath[commands[3]] = commands[4];
+                        msg = "file uploaded successfully by user ID: "+curr_user;
+                    }
+                }
+                else{
+                    file fl;
+                    fl.file_size = stoi(words[4]);
+                    fl.file_name = words[2];
+                    fl.file_users.push_back({curr_user,1});
+                    fl.file_hash = words[6];
+                    // users[commands[2]]->fnameToPath[commands[3]] = commands[4];
+                    for(int i=7; i<words.size(); i++)
+                        fl.chunk_hashes.push_back(words[i]);
+                    file_map[words[2]] = fl;
+
+                    msg = "file uploaded successfully by user ID: "+curr_user;
+                }
             }
             send(newsockfd, msg.c_str(), msg.size(), 0);
         }
@@ -220,10 +305,6 @@ int main(int argc, char *argv[]) {
         tracker_list.push_back({ip, port});
     }
     file.close();
-    
-    int sync_port = tracker_list[1].port;
-    if (sno == 1)
-        sync_port = tracker_list[0].port;
 
     int sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd < 0)
@@ -243,7 +324,13 @@ int main(int argc, char *argv[]) {
         error("Error while listening");
 
     socklen_t cli_len = sizeof(client_addr);
-    thread t1;
+    vector<thread> threads_list;
+    thread quit_thread(quit_func, 1);
+    char buffer[256];
+    int log_fd = open("tracker_log.txt", O_RDWR | O_APPEND | O_CREAT, 0644);
+    if(log_fd < 0)
+        error("Unable to open the file");
+
     while(1) {
         int newsockfd = accept(sockfd, (struct sockaddr *) &client_addr, &cli_len);
         if (newsockfd < 0) {
@@ -251,19 +338,14 @@ int main(int argc, char *argv[]) {
             continue;
         }
 
-        t1 =  thread(client_handler, newsockfd);
+        threads_list.push_back(thread(client_handler,newsockfd));
         cout << "client connection success" << endl;
-       
     }
-    t1.join();
+    for(int i=0;i<threads_list.size(); i++)
+		threads_list[i].join();
+        
+    close(log_fd);
 
-        // char buffer[256];
-        // int log_fd = open("tracker_log.txt", O_RDWR | O_APPEND | O_CREAT, 0644);
-        // if(log_fd < 0)
-        // error("Unable to open the file"); 
-
-
-    // close(log_fd);
     close(sockfd);
     return 0;
 }
